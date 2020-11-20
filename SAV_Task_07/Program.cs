81598a9f5882e9850e276aa4fcef544e96acc92c
StekolnikovaAleksandra <74679525+StekolnikovaAleksandra@users.noreply.github.com>
2020-11-20 18:22:24 +0300

    TrainingPractice_01
    TrainingPractice_01

---
+++ SAV_Task_07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SAV_Task_07
+{
+    class Program
+    {
+        private static void Shuffle(int[] permutationmass)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var rndInd1 = rand.Next(0, permutationmass.Length);
+                var rndInd2 = rand.Next(0, permutationmass.Length);
+                var temp = permutationmass[rndInd1];
+                permutationmass[rndInd1] = permutationmass[rndInd2];
+                permutationmass[rndInd2] = temp;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int size;
+            Console.Write("Введите размерность массива: ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            int[] mass1 = new int[size];
+
+            Random rand = new Random();
+            for (int y = 0; y < size; y++)
+            {
+                mass1[y] = rand.Next(1, 50);
+            }
+
+            int[] mass2 = mass1;
+
+            Console.WriteLine("Изначальный массив:");
+            Console.WriteLine(string.Join(" ", mass1));
+            Shuffle(mass2);
+            Console.WriteLine("Обработанный массив:");
+            Console.WriteLine(string.Join(" ", mass2));
+            Console.ReadKey();
+        }
+    }
+}
